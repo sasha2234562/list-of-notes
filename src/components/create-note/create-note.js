91343a73c3716e9c9c2ceae105bd24ad3be74540
Sasha <skalitsenia@mail.ru>
2023-12-08 22:52:03 +0300

    refactoring, Error

--- src/components/create-note/create-note.js
+++ src/components/create-note/create-note.js
@@ -1,35 +1,47 @@
 import {addNote} from "../../store/reduser";
 import {useState} from "react";
 import {useDispatch} from "react-redux";
-import {Button} from "@mui/material";
+import {Button, TextField} from "@mui/material";
 import n from "./create-note.module.css"
 
 export const CreateNote = (props) => {
     const [title, setTitle] = useState('')
+    const [error, setError] = useState(false)
     const dispatch = useDispatch()
-    let [arr,serArr] = useState([])
+    let [arr, serArr] = useState([])
     const onChangeHandler = (e) => {
         setTitle(e.currentTarget.value);
-       let arr = props.notes.filter(n => n.title.includes('#'));
+        let arr = props.notes.filter(n => n.title.includes('#'));
         serArr(arr)
+        setError(false)
     }
     console.log(arr)
     const createNote = () => {
-        if (title.length !== 0) {
+        if (title.length >= 3) {
             dispatch(addNote(title, props.listId))
             setTitle('')
+        } else {
+            setError(true)
         }
     }
     return (
 
         <>
             <div className={n.containerCreateNote}>
-                <input onChange={onChangeHandler} value={title}/>
+                <TextField
+                    className={n.textFielD}
+                    error={error}
+                    id="outlined-error"
+                    onChange={onChangeHandler}
+                    value={title}
+                />
+                {/*<input onChange={onChangeHandler} value={title}/>*/}
                 <Button variant="contained" color="success" onClick={createNote}>
                     +
                 </Button>
             </div>
-            <div>{arr.map(n=> <div>{n.title}</div>)}</div>
+            {error && <div className={n.error}>If the note length should be at least 3 characters.</div>}
+            <div>{arr.map(n => <div>{n.title}</div>)}</div>
         </>
     )
 }
